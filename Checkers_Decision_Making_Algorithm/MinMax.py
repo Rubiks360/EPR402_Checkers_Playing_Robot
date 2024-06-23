1ec98128fd13bb33380b66f33ad000be8b8ed41a
Rubiks360 <u04515685@tuks.co.za>
2024-06-23 17:57:18 +0200

    Update MinMax.py
    Added the board scoring into the algorithm with the returned best move

--- Checkers_Decision_Making_Algorithm/MinMax.py
+++ Checkers_Decision_Making_Algorithm/MinMax.py
@@ -4,18 +4,20 @@ def temp_state_score() -> int:
     return np.random.randint(-25, 26)
 
 
-def calculate_move_min_max(depth: int, max_depth: int, my_game, game_state: np.ndarray, max_player_turn: bool) -> int:
+def calculate_move_min_max(depth: int, max_depth: int, my_game, game_state: np.ndarray, max_player_turn: bool, alpha: int, beta: int) -> tuple:
     # return score, move
 
+    saved_moves = []
     saved_scores = []
+    best_move = []
 
     if depth == 0:
         # now at the deepest tree point
+        board_score = my_game.score_board(game_state)
         # return the current game state score
-        return temp_state_score()
+        return board_score, best_move
     else:
         # go down one deeper
-        # calculate all possible moves from the current state
         all_piece_moves = my_game.calculate_side_possible_moves(max_player_turn, my_game.board.copy())
 
         if len(all_piece_moves) > 0:
@@ -24,8 +26,11 @@ def calculate_move_min_max(depth: int, max_depth: int, my_game, game_state: np.n
             max_score = -25
             min_score = 25
 
+            end_search = False
             # for each piece that can move
             for p in all_piece_moves:
+                if end_search:
+                    break
 
                 # for each move of the piece
                 for m in p:
@@ -33,28 +38,39 @@ def calculate_move_min_max(depth: int, max_depth: int, my_game, game_state: np.n
                     new_state = my_game.execute_move(m, game_state.copy())
 
                     if max_player_turn:
-                        returned_score = calculate_move_min_max(depth - 1, max_depth, my_game, new_state.copy(), False)
+                        returned_score, _ = calculate_move_min_max(depth - 1, max_depth, my_game, new_state.copy(),
+                                                                   False, alpha, beta)
                     else:
-                        returned_score = calculate_move_min_max(depth - 1, max_depth, my_game, new_state.copy(), True)
+                        returned_score, _ = calculate_move_min_max(depth - 1, max_depth, my_game, new_state.copy(),
+                                                                   True, alpha, beta)
 
                     if depth == max_depth:
+                        saved_moves.append(m)
                         saved_scores.append(returned_score)
 
                     if max_player_turn:
                         max_score = max(max_score, returned_score)
+                        alpha = max(alpha, returned_score)
+                        if beta <= alpha:
+                            end_search = True
+                            break
+
                     else:
                         min_score = min(min_score, returned_score)
+                        beta = min(beta, returned_score)
+                        if beta <= alpha:
+                            end_search = True
+                            break
 
             if depth != max_depth:
                 # after checking each child value, returning the child score
                 if max_player_turn:
-                    return max_score
+                    return max_score, []
                 else:
-                    return min_score
+                    return min_score, []
             else:
                 # if at the first minmax call
 
-                # find move equal to best return move
                 if max_player_turn:
                     # looking for max value
                     best_score = max(saved_scores)
@@ -62,12 +78,21 @@ def calculate_move_min_max(depth: int, max_depth: int, my_game, game_state: np.n
                     # looking for min value
                     best_score = min(saved_scores)
 
-                print("All scores =", saved_scores)
-                print("best score =", best_score)
+                # save the best moves in the selection list
+                best_moves = []
+                for score_index in range(len(saved_scores)):
+                    if saved_scores[score_index] == best_score:
+                        best_moves.append(saved_moves[score_index])
+
+                # select one of best moves at random
+                selected_move = np.random.randint(0, len(best_moves))
+
+                # OR select the first equally best move
+                # selected_move = 0
 
                 # only returning the best move
-                return best_score
+                return best_score, best_moves[selected_move]
 
         else:
             # no possible moves for current player
-            return temp_state_score()
+            return my_game.score_board(game_state), best_move
